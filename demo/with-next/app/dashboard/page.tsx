1b35742b8e71cd10c89c960803545c8ccdf2b26d
Lucas <lucas_walk@hotmail.com>
2025-07-13 12:39:29 -0300

    arrumando importação minikit

--- demo/with-next/app/dashboard/page.tsx
+++ demo/with-next/app/dashboard/page.tsx
@@ -17,7 +17,7 @@ import {
 import axios from "axios";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MiniKit from '@worldcoin/minikit-js';
+import { MiniKit } from '@worldcoin/minikit-js';
 import ClaimButton from '@/components/ClaimButton';
 
 interface UserProfile {
