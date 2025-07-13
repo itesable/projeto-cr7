f7c8a5c8b4bb3854fdef4f371644e19f35e700bd
Lucas <lucas_walk@hotmail.com>
2025-07-13 10:38:28 -0300

    adicionando modulo axios e removendo pasta test

--- demo/with-next/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SearchParams } from '@/components/ClientContent/SearchParams';
-
-export default function TestPage() {
-  return (
-    <div className="bg-white text-black min-h-full p-5">
-      <SearchParams />
-    </div>
-  );
-}
